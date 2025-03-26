3392003b142866395edcd68491dc6770b0a2c26a
NickTTD <nico-diaz@hotmail.es>
2025-03-26 18:33:54 -0300

    factorial 2.0

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -6,13 +6,13 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
+
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
         return 0
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
         while(num > 1): 
@@ -20,9 +20,20 @@ def factorial(num):
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+# Check if the user provided a command-line argument
+if len(sys.argv) == 1:
+    # No command-line argument, ask for input
+    try:
+        num = int(input("Ingrese un número: "))
+    except ValueError:
+        print("Por favor ingrese un número válido.")
+        sys.exit()
+else:
+    # Use the argument as the number
+    try:
+        num = int(sys.argv[1])
+    except ValueError:
+        print("Por favor ingrese un número válido.")
+        sys.exit()
 
+print(f"Factorial {num}! es {factorial(num)}")
