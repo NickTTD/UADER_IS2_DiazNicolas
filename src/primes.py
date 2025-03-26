c83adce1e65c82dbc48a4bd7a8ad8777d355bca8
NickTTD <nico-diaz@hotmail.es>
2025-03-26 18:15:40 -0300

    Added comments

--- src/primes.py
+++ src/primes.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 # Python program to display all the prime numbers within an interval
 
+
+# Define the lower and upper bounds of the interval
 lower = 1
 upper = 500
 
+# Print the range for which we are finding prime numbers
 print("Prime numbers between", lower, "and", upper, "are:")
 
+# Iterate through each number in the range from lower to upper (inclusive)
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
+    # Prime numbers are greater than 1, so we start checking from 2
+    if num > 1:
+        # Check for factors of num starting from 2 up to num-1
+        for i in range(2, num): 
+            # If num is divisible by i, it is not a prime number
+            if (num % i) == 0:
+                # If we find a divisor, break the loop
+                break
+        else:
+            # If no divisors are found, num is a prime number and we print it
+            print(num)
